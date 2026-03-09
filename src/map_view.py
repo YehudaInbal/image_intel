5db948882edcab27d4043ee738691a7ddf0053fe
Pesach Bagad <bgdpsh@gmail.com>
2026-03-09 12:43:40 +0200

    final results in first day

--- src/map_view.py
+++ src/map_view.py
@@ -14,10 +14,10 @@ map_view.py - יצירת מפה אינטראקטיבית
 """
 
 
-
-
 import folium
-
+import webbrowser
+import os
+from extractor import extract_all
 
 def sort_by_time(arr):
     return sorted(arr, key=lambda x: (x.get("datetime") is None, x.get("datetime")))
@@ -104,31 +104,17 @@ def create_map(images_data):
 
 
 if __name__ == "__main__":
-    fake_data = [
-        {
-            "filename": "test1.jpg",
-            "latitude": 32.0853,
-            "longitude": 34.7818,
-            "has_gps": True,
-            "camera_make": "Samsung",
-            "camera_model": "Galaxy S23",
-            "datetime": "2025-01-12 08:30:00",
-        },
-        {
-            "filename": "test2.jpg",
-            "latitude": 31.7683,
-            "longitude": 35.2137,
-            "has_gps": True,
-            "camera_make": "Apple",
-            "camera_model": "iPhone 15 Pro",
-            "datetime": "2025-01-13 09:00:00",
-        },
-    ]
+    data = extract_all("../images")
 
-    html = create_map(fake_data)
-    with open("test_map.html", "w", encoding="utf-8") as f:
+    html = create_map(data)
+
+    file_name = "test_map.html"
+    with open(file_name, "w", encoding="utf-8") as f:
         f.write(html)
 
-    print("Map saved to test_map.html")
+    print(f"Map saved to {file_name}")
+
+    full_path = os.path.abspath(file_name)
+    webbrowser.open(f"file://{full_path}")
 
 
